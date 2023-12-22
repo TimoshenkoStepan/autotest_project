5d7417e12cee693dad5f7b42805bb7f046502109
Stepan <timoshenko.stepa@mail.ru>
2023-12-23 06:48:53 +0700

    Preparations for the review

--- test_product_page.py
+++ test_product_page.py
@@ -18,7 +18,8 @@ class TestUserAddToBasketFromProductPage():
         login_page.should_be_authorized_user()
         self.page = ProductPage(browser, link)
         self.page.open()
-    
+
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self):
         self.page.add_to_basket()
         self.page.solve_quiz_and_get_code()
@@ -28,6 +29,7 @@ class TestUserAddToBasketFromProductPage():
     def test_user_cant_see_success_message(self):
         self.page.should_not_be_success_message()
 
+@pytest.mark.need_review
 def test_guest_can_add_product_to_basket(browser):
     page = ProductPage(browser, link)
     page.open()
@@ -36,6 +38,7 @@ def test_guest_can_add_product_to_basket(browser):
     page.compare_name_and_name_added_to_basket()
     page.compare_price_and_price_to_basket()
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     page = ProductPage(browser, link)
     page.open()
@@ -72,6 +75,7 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.open()
     page.should_be_login_link()
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
